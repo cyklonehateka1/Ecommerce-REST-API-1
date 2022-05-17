41a926abc3ae5c0a296c4e607b4fbae82f439d32
Emmanuel Hateka <emmanuelhateka.com>
2022-05-17 15:44:16 +0000

    started with the user models

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+
 dotenv.config();
 
 mongoose
@@ -11,6 +13,10 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use(express.json());
+
+app.use("/api/users", userRoute);
 
 app.listen(5000, () => {
   console.log("Backend server is runing");
